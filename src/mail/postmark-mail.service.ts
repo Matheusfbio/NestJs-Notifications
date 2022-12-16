3ed79f952d76256687ca381a193680e216de796b
Matheus Fabio <fabiomatheus488@gmail.com>
2022-12-15 22:51:22 -0300

    Update of the project notifications-service

--- src/mail/postmark-mail.service.ts
+++ src/mail/postmark-mail.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from "@nestjs/common";
 import { MailService } from "./mail.service";
 
 @Injectable()
-export class SMTPMailService implements MailService {
+export class PostmarkMailService implements MailService {
   sendEmail(): string {
-    return "SMTP Mail!";
+    return "Postmark Mail!";
   }
 }
