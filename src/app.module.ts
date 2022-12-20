0e5acc33a61fd6b504081b9c956e4975b2ec9e7a
Matheus Fabio <fabiomatheus488@gmail.com>
2022-12-20 13:35:39 -0300

    Validation of requisition's body

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
-import { MailService } from "./mail/mail.service";
-import { PostmarkMailService } from "./mail/postmark-mail.service";
-import { SMTPMailService } from "./mail/smtp-mail.service";
+import { PrismaService } from "./prisma.service";
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [
-    {
-      provide: MailService,
-      useClass: PostmarkMailService,
-    },
-  ],
+  providers: [PrismaService],
 })
-export class AppModule {}
+// eslint-disable-next-line prettier/prettier
+export class AppModule { }
